970712703b8ad0fc679335f6d6fd39478384ee95
2894638479 <2894638479@qq.com>
2025-02-27 15:39:33 +0800

    钻石块demo

---
+++ src/client/kotlin/org/schoolustc/SchoolUSTCClient.kt
@@ -0,0 +1,9 @@
+package org.schoolustc
+
+import net.fabricmc.api.ClientModInitializer
+
+object SchoolUSTCClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/org/schoolustc/SchoolUSTC.kt
@@ -0,0 +1,20 @@
+package org.schoolustc
+
+import net.fabricmc.api.ModInitializer
+import org.schoolustc.SchoolUSTC.id
+import org.slf4j.LoggerFactory
+
+
+object SchoolUSTC : ModInitializer {
+	val id = "school-ustc"
+    val logger = LoggerFactory.getLogger(id)
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		DiamondStructure.register()
+		DiamondStructurePiece.register()
+		logger.info("Hello Fabric world!")
+	}
+}
