4afb003f279cea4f8c8d87f2d293a1a8dae6f175
2894638479 <2894638479@qq.com>
2025-06-01 14:29:35 +0800

    修复食堂物品显示

--- src/main/kotlin/org/schoolustc/items/FoodMachineBlockEntity.kt
+++ src/main/kotlin/org/schoolustc/items/FoodMachineBlockEntity.kt
@@ -3,10 +3,13 @@ package org.schoolustc.items
 import net.fabricmc.fabric.api.`object`.builder.v1.block.entity.FabricBlockEntityTypeBuilder
 import net.minecraft.core.BlockPos
 import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.protocol.Packet
+import net.minecraft.network.protocol.game.ClientGamePacketListener
 import net.minecraft.world.item.Items
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import org.schoolustc.MyRegister
+import org.schoolustc.logger
 import org.schoolustc.structureDsl.read
 import org.schoolustc.structureDsl.write
 
@@ -19,7 +22,15 @@ class FoodMachineBlockEntity(
         fun register() = MyRegister.registerBlockEntity("food_machine", type)
     }
     var item = Items.AIR
+        set(value) {
+            field = value
+            setChanged()
+        }
     var cost = 0
+        set(value) {
+            field = value
+            setChanged()
+        }
     override fun saveAdditional(tag: CompoundTag) {
         super.saveAdditional(tag)
         tag.write("i",item)
@@ -30,5 +41,9 @@ class FoodMachineBlockEntity(
         super.load(tag)
         item = tag.read("i")
         cost = tag.read("c")
+    }
+
+    override fun getUpdateTag(): CompoundTag {
+        return saveWithoutMetadata()
     }
 }
